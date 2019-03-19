2fea3087ffc722816b3fb350043069a78aec7040
Andriy Legeta <andriylegeta@gmail.com>
2019-03-19 19:34:53 +0200

    1 commit finish register form, add components: toolbar, navigation and sidenav, update welcome component, add tabs to training component

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import { WelcomeComponent } from "../welcome/welcome.component";
+import {RegisterComponent} from "../authorization/register/register.component";
+import {LoginComponent} from "../authorization/login/login.component";
+import {TrainingComponent} from "../training/training.component";
+
+const  routes: Routes =[
+  { path: '', component: WelcomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'training', component: TrainingComponent },
+
+  ];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/material.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from "@angular/core";
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatDatepickerModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatNativeDateModule,
+  MatSidenavModule, MatTabsModule,
+  MatToolbarModule
+} from "@angular/material";
+
+
+
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatTabsModule
+
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatTabsModule
+  ]
+})
+export class MaterialModule { }
